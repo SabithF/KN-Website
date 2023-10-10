b0c1f32ab90888b5e11ac5cc81d68cac6a048cb3
IT Analyst <itanalyst@cjm.lk>
2023-10-11 00:09:34 +0530

    Review slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+// Image slider
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: "smooth"});
+
+            
+        });
+    });
+}
+
+window.addEventListener("load", initSlider);
